d9c6910a65784640b235f69f7caa2776eb03f760
Ganjar Hadiatna <ganjarhadiatna.gh@gmail.com>
2019-04-01 21:43:44 +0700

    update db_new.sql engine

--- db_new.sql
+++ db_new.sql
@@ -282,7 +282,7 @@ CREATE TABLE `users` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
