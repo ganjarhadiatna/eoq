35489af92cd5fd80e17c0ec7611f4959043c148f
Ganjar Hadiatna <ganjarhadiatna.gh@gmail.com>
2019-07-14 18:20:00 +0700

    fixing ui penjualan

--- resources/views/penjualan/create.blade.php
+++ resources/views/penjualan/create.blade.php
@@ -7,41 +7,50 @@
 		</div>
 
 		<div class="card-body">
-			<form 
+        <div class="row">
+            <div class="col-sm">
+                <form 
                 id="formSubmit" 
                 method="post" 
                 action="javascript:void(0)" 
                 autocomplete="off">
                 @csrf
 
-                <div>
-                    <h3 class="mb-0">Data Barang</h3>
-                </div>
 
-                <div>
-
-                    <div class="form-group{{ $errors->has('kode_transaksi') ? ' has-danger' : '' }}">
-                        <label class="form-control-label" for="kode_transaksi">{{ __('Kode Transaksi (hanya satu kali pengisian) *') }}</label>
-                        <input 
-                            type="text" 
-                            name="kode_transaksi" 
-                            id="kode_transaksi" 
-                            class="form-control form-control-alternative{{ $errors->has('kode_transaksi') ? ' is-invalid' : '' }}" 
-                            placeholder="{{ __('AAA-000') }}"
-                            min="0"
-                            required>
-                            @if ($errors->has('kode_transaksi'))
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $errors->first('kode_transaksi') }}</strong>
-                                </span>
-                            @endif
+                    <div>
+                        <h3 class="mb-0">Kode Transaksi</h3>
                     </div>
+
+                    <div>
+                        <div class="form-group{{ $errors->has('kode_transaksi') ? ' has-danger' : '' }}">
+                            <label class="form-control-label" for="kode_transaksi">{{ __('Kode transaksi (hanya satu kali pengisian) *') }}</label>
+                            <input 
+                                type="text" 
+                                name="kode_transaksi" 
+                                id="kode_transaksi" 
+                                class="form-control form-control-alternative{{ $errors->has('kode_transaksi') ? ' is-invalid' : '' }}" 
+                                placeholder="{{ __('AAA-000') }}"
+                                min="0"
+                                required>
+                                @if ($errors->has('kode_transaksi'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('kode_transaksi') }}</strong>
+                                    </span>
+                                @endif
+                        </div>
+                    </div>
+
+                    <div>
+
+                        <div>
+                            <h3 class="mb-0">Data Barang</h3>
+                        </div>
 
                         <div class="row mb-2">
 
                             <div class="col-sm">
                                 <div class="form-group{{ $errors->has('idbarang') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="idbarang">{{ __('Pilih Barang *') }}</label>
+                                    <label class="form-control-label" for="idbarang">{{ __('Pilih barang *') }}</label>
                                     <select 
                                         name="idbarang" 
                                         id="idbarang" 
@@ -82,7 +91,7 @@
 
                         </div>
 
-                        <div class="row mb-5">
+                        <div class="row mb-3">
 
                             <div class="col-sm">
                                 <div class="form-group{{ $errors->has('stok_barang') ? ' has-danger' : '' }}">
@@ -98,8 +107,6 @@
                                         disabled 
                                         required>
                                 </div>
-                            </div>
-                            <div class="col-sm">
                                 <div class="form-group{{ $errors->has('sisa_stok') ? ' has-danger' : '' }}">
                                     <label class="form-control-label" for="sisa_stok">{{ __('Sisa stok') }}</label>
                                     <input 
@@ -117,7 +124,7 @@
                             <div class="col-sm">
 
                                 <div class="form-group{{ $errors->has('harga_barang') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="harga_barang">{{ __('Harga Barang') }}</label>
+                                    <label class="form-control-label" for="harga_barang">{{ __('Harga barang') }}</label>
                                     <input 
                                         type="number" 
                                         name="harga_barang" 
@@ -132,11 +139,8 @@
                                             </span>
                                         @endif
                                 </div>
-                            </div>
-
-                            <div class="col-sm">
                                 <div class="form-group{{ $errors->has('total_biaya') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="total_biaya">{{ __('Total Biaya') }}</label>
+                                    <label class="form-control-label" for="total_biaya">{{ __('Total biaya') }}</label>
                                     <input 
                                         type="number" 
                                         name="total_biaya" 
@@ -154,16 +158,74 @@
 
                             </div>
                             <div class="col-sm">
-                                <label class="form-control-label">Simpan?</label>
+                                <label class="form-control-label">Tambah?</label>
                                 <div>
                                     <button 
                                         type="submit" 
-                                        class="btn btn-primary">Simpan Penjualan</button>
+                                        class="btn btn-primary">Tambah Barang</button>
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-xl-5">
+                <div>
+                    <h3 class="mb-0">Data Penjualan</h3>
+                    <label class="form-control-label">{{ __('Data ini tidak akan disimpan di database *') }}</label>
                 </div>
-            </form>
+                <div class="form-group{{ $errors->has('total_penjualan') ? ' has-danger' : '' }}">
+                    <label class="form-control-label" for="total_penjualan">{{ __('Total biaya penjualan') }}</label>
+                    <input 
+                        type="text" 
+                        name="total_penjualan" 
+                        id="total_penjualan" 
+                        class="form-control form-control-alternative{{ $errors->has('total_penjualan') ? ' is-invalid' : '' }}" 
+                        placeholder="{{ __('0') }}"
+                        min="0"
+                        readonly="true" 
+                        required>
+                        @if ($errors->has('total_penjualan'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('total_penjualan') }}</strong>
+                            </span>
+                        @endif
+                </div>
+                <div class="form-group{{ $errors->has('jumlah_uang') ? ' has-danger' : '' }}">
+                    <label class="form-control-label" for="jumlah_uang">{{ __('Jumlah uang') }}</label>
+                    <input 
+                        type="text" 
+                        name="jumlah_uang" 
+                        id="jumlah_uang" 
+                        class="form-control form-control-alternative{{ $errors->has('jumlah_uang') ? ' is-invalid' : '' }}" 
+                        placeholder="{{ __('0') }}"
+                        min="0"
+                        required>
+                        @if ($errors->has('jumlah_uang'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('jumlah_uang') }}</strong>
+                            </span>
+                        @endif
+                </div>
+                <div class="form-group{{ $errors->has('jumlah_kembalian') ? ' has-danger' : '' }}">
+                    <label class="form-control-label" for="jumlah_kembalian">{{ __('Jumlah kembalian') }}</label>
+                    <input 
+                        type="text" 
+                        name="jumlah_kembalian" 
+                        id="jumlah_kembalian" 
+                        class="form-control form-control-alternative{{ $errors->has('jumlah_kembalian') ? ' is-invalid' : '' }}" 
+                        placeholder="{{ __('0') }}"
+                        min="0"
+                        readonly="true" 
+                        required>
+                        @if ($errors->has('jumlah_kembalian'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('jumlah_kembalian') }}</strong>
+                            </span>
+                        @endif
+                </div>
+            </div>
+        </div>
 
             <div>
                 <h3 class="mb-0">Daftar Barang</h3>
@@ -227,6 +289,7 @@
             })
             .done(function(data) {
                 var dt = '';
+                var total_penjualan = 0;
                 for (var i = 0; i < data.length; i++) {
                     dt += '\
                     <tr>\
@@ -247,9 +310,13 @@
                         </td>\
                     </tr>\
                     ';
+                    total_penjualan += data[i].total_biaya;
                 }
 
-                $('#get-result').html(dt)
+                $('#get-result').html(dt);
+                $('#total_penjualan').val(total_penjualan);
+                $('#jumlah_uang').val('');
+                $('#jumlah_kembalian').val('');
             })
             .fail(function(e) {
                 console.log("error " + e.responseJSON.message);
@@ -362,6 +429,13 @@
 
             $('#kode_transaksi').keyup(function(event) {
                 getData($('#kode_transaksi').val());
+            });
+
+            $('#jumlah_uang').keyup(function(event) {
+                var jumlah_uang = $('#jumlah_uang').val();
+                var total_penjualan = $('#total_penjualan').val();
+                var temp = jumlah_uang - total_penjualan;
+                $('#jumlah_kembalian').val(temp);
             });
 
         });
